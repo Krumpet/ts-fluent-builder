19af967ca6376dbaed4e1c26b14d96c96af09e9d
Ran Dov Lottem <ran.lottem@mail.huji.ac.il>
2022-08-29 23:08:45 +0300

    cleanup paths added utility types

--- src/gradual-builder.ts
+++ src/gradual-builder.ts
@@ -4,7 +4,17 @@ type BuilderAux<T, AlreadyBuilt> = Exclude<keyof T, AlreadyBuilt> extends never 
     [K in Exclude<keyof T, AlreadyBuilt> & string as `set${Capitalize<K>}`]: (arg: T[K]) => BuilderAux<T, AlreadyBuilt | K>
 }
 
-type shouldBeNever = Exclude<keyof { 'omg': string, 'hello': string }, 'hello' | 'omg'>;
+// Taken from https://stackoverflow.com/a/52991061/6338059
+type RequiredKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? never : K }[keyof T];
+type OptionalKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? K : never }[keyof T];
+
+type RequiredLiteralKeys<T> = keyof { [K in keyof T as string extends K ? never : number extends K ? never :
+    {} extends Pick<T, K> ? never : K]: 0 }
+
+type OptionalLiteralKeys<T> = keyof { [K in keyof T as string extends K ? never : number extends K ? never :
+    {} extends Pick<T, K> ? K : never]: 0 }
+
+type IndexKeys<T> = string extends keyof T ? string : number extends keyof T ? number : never;
 
 export type Builder<T> = BuilderAux<T, never>;
 
@@ -19,9 +29,11 @@ export function createBuilder<T>(): Builder<T> {
         builder["set" + prop.name.charAt(0).toUpperCase() + prop.name.slice(1)] = function (arg: any) { result[prop.name] = arg; return builder; }
     });
     builder["build"] = () => {
-        const finishedObject: any = {};
-        type.getProperties().forEach(prop => finishedObject[prop.name] = result[prop.name]);
-        return finishedObject;
+        // TODO: create new copy?
+        return result;
+        // const finishedObject: any = {};
+        // type.getProperties().forEach(prop => finishedObject[prop.name] = result[prop.name]);
+        // return finishedObject;
     }
 
     return builder as any;
