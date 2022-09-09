76db77c548fd0160773bbe889fca7f8044ddaf51
Ran Dov Lottem <ran.lottem@mail.huji.ac.il>
2022-09-10 00:51:12 +0300

    more tests and readme

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ts-fluent-builder
-a strictly-typed builder generator for typescript types
+## a strictly-typed builder generator for typescript types
+
+A fluent builder allows chaining calls to the methods that set properties:
+
+```typescript
+interface ExampleType {
+    typeName: string;
+    id: number;
+    isBest: boolean;
+}
+
+const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+const finishedObject = exampleBuilder.setTypeName("goodbye").setId(2).setIsBest(true).build();
+```
+
+The builder API usually allows for multiple calls to the same setter method, as well as calling the `build()` method before all required properties have been set, resulting in a runtime error.
+
+We can leverage typescript's type system to produce a builder type that:
+* Only allows setting each property once
+* Only allows calling `build()` once every required property has been set
+
+```typescript
+const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+exampleBuilder.build();
+              ~~~~~~~~
+// Property 'build' does not exist on type 'Builder<ExampleType>'.
+
+exampleBuilder.setId(2).setId(1);
+                       ~~~~~~~~~
+// Property 'setId' does not exist on type 'BuilderAux<ExampleType, "id">'
+```
+
+We can also create partial objects, either with a loose `Partial<T>` type or with a stricter type that tracks which properties have already been set.
+
+**Partial object:**
+```typescript
+const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+const partial: Partial<ExampleType> = exampleBuilder.buildPartial();
+```
+
+**Partial object with stricter typing:**
+```typescript
+const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+const builderAfterNameSet = exampleBuilder.setTypeName("hello");
+const partialTyped: Pick<ExampleType, "typeName"> = builderAfterNameSet.buildPartialTyped();
+```
--- test/gradual-builder.test.ts
+++ test/gradual-builder.test.ts
@@ -1,26 +1,59 @@
 import { createBuilder } from "../src/gradual-builder";
 import { Builder } from "../src/types/types";
 
+interface ExampleType {
+    typeName: string;
+    id: number;
+    isBest: boolean;
+}
+
 test('create a builder and object from it', () => {
-    interface ExampleType {
-        typeName: string;
-        id: number;
-        isBest: boolean;
-    }
-    
     const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
-    
     const builderAfterNameSet = exampleBuilder.setTypeName("goodbye");
-
-    // TODO: move to new test
-    const partial = exampleBuilder.buildPartial();
-
-    const partialTyped = builderAfterNameSet.buildPartialTyped();
-    partialTyped.typeName;
-    
     const finishedSetting = builderAfterNameSet.setId(2).setIsBest(true);
-    
+
     const isExampleType: ExampleType = finishedSetting.build();
-    
+
     expect(isExampleType.id).toEqual(2);
-})
+});
+
+test('create a builder and build a partial object', () => {
+    const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+    const partial: Partial<ExampleType> = exampleBuilder.buildPartial();
+
+    expect(partial.id).toBeUndefined();
+    expect(partial.isBest).toBeUndefined();
+    expect(partial.typeName).toBeUndefined();
+});
+
+test('create a builder and build a partial object (typed)', () => {
+    const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+    const builderAfterNameSet = exampleBuilder.setTypeName("hello");
+    const partialTyped = builderAfterNameSet.buildPartialTyped();
+
+
+    expect(partialTyped.typeName).toBe("hello");
+
+    // @ts-expect-error
+    expect(partialTyped.id).toBeUndefined();
+
+    // @ts-expect-error
+    expect(partialTyped.isBest).toBeUndefined();
+});
+
+test('the builder creates a new object on each build invocation', () => {
+    const exampleBuilder: Builder<ExampleType> = createBuilder<ExampleType>();
+    const fullySetBuilder = exampleBuilder.setTypeName("goodbye").setId(2).setIsBest(true);
+    const exampleInstance = fullySetBuilder.build();
+
+    expect(exampleInstance.typeName).toBe("goodbye");
+    expect(exampleInstance.id).toBe(2);
+    expect(exampleInstance.isBest).toBe(true);
+
+    exampleInstance.isBest = false;
+
+    const exampleInstanceAgain = fullySetBuilder.build();
+
+    expect(exampleInstance.isBest).toBe(false);
+    expect(exampleInstanceAgain.isBest).toBe(true);
+});
